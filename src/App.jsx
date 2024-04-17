a361a261e2b56b2912c556f848fb652cede5851e
soohian <shonendumm@gmail.com>
2024-04-17 21:32:56 +0800

    Add flex styling

--- src/App.jsx
+++ src/App.jsx
@@ -17,10 +17,15 @@ function App() {
           <ListCast onChoice={(info)=> {setMemberInfo(info)}} />
           { memberInfo &&  
             <article><hgroup>
-              <div>
-                <img src={`images/${memberInfo.slug}.svg`} alt={memberInfo.name}/>
+              <div style={{
+                display:"flex",
+                gap: "1rem",
+            }}>
+                <img style={{width:"200px"}} src={`images/${memberInfo.slug}.svg`} alt={memberInfo.name}/>
+                <hgroup>
                 <h1>{memberInfo.name}</h1>
                 <p>{memberInfo.bio}</p>
+                </hgroup>
               </div>
               </hgroup></article>
           }
