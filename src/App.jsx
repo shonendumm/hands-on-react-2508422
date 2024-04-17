6315a65f78bb3dc10f2ea0023ddb88998c32046f
soohian <shonendumm@gmail.com>
2024-04-17 21:12:00 +0800

    Pass event with data from component to component

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,32 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import Welcome from './components/Welcome';
 import Support from './components/Support';
 import ListCast from './components/ListCast';
 
 function App() {
   const name = 'StarGazers'
+  let [memberInfo, setMemberInfo] = useState(null);
+
   return (
     <div className="container">
-      <article>
+      
         <hgroup>
           <img src="images/group.svg" alt="StarGazers Group" />
           <Welcome name="StarGazers" />
-          <p>Members of an <b>intergalactic alliance</b><br />
-            paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
-          <ListCast />
+          <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
+          <ListCast onChoice={(info)=> {setMemberInfo(info)}} />
+          { memberInfo &&  
+            <article><hgroup>
+              <div>
+                <img src={`images/${memberInfo.slug}.svg`} alt={memberInfo.name}/>
+                <h1>{memberInfo.name}</h1>
+                <p>{memberInfo.bio}</p>
+              </div>
+              </hgroup></article>
+          }
           <Support />
         </hgroup>
-      </article>
+      
     </div>
   )
 }
