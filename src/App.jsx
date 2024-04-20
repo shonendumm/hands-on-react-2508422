3d647947f5e9ccae538472ac42e6624e0adecbdb
soohian <shonendumm@gmail.com>
2024-04-20 10:53:12 +0800

    Refactor infosection

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,32 @@
-import React, { Component, useState } from 'react';
-import Welcome from './components/Welcome';
-import Support from './components/Support';
-import ListCast from './components/ListCast';
+import React, { Component, useState } from "react";
+import Welcome from "./components/Welcome";
+import Support from "./components/Support";
+import ListCast from "./components/ListCast";
+import InfoSection from "./components/InfoSection";
 
 function App() {
-  const name = 'StarGazers'
+  const name = "StarGazers";
   let [memberInfo, setMemberInfo] = useState(null);
 
   return (
     <div className="container">
-      
-        <hgroup>
-          <img src="images/group.svg" alt="StarGazers Group" />
-          <Welcome name="StarGazers" />
-          <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
-          <ListCast onChoice={(info)=> {setMemberInfo(info)}} />
-          { memberInfo &&  
-            <article><hgroup>
-              <div style={{
-                display:"flex",
-                gap: "1rem",
-            }}>
-                <img style={{width:"200px"}} src={`images/${memberInfo.slug}.svg`} alt={memberInfo.name}/>
-                <hgroup>
-                <h1>{memberInfo.name}</h1>
-                <p>{memberInfo.bio}</p>
-                </hgroup>
-              </div>
-              </hgroup></article>
-          }
-          <Support />
-        </hgroup>
-      
+      <hgroup>
+        <img src="images/group.svg" alt="StarGazers Group" />
+        <Welcome name="StarGazers" />
+        <p>
+          Members of an <b>intergalactic alliance</b> paving the way for peace
+          and benevolence among all species. They are known for their enthusiasm
+          for science, for their love of fun, and their dedication to education.
+        </p>
+        <ListCast
+          onChoice={(info) => {
+            setMemberInfo(info);
+          }}
+        />
+        {memberInfo && <InfoSection memberInfo={memberInfo} />}
+        <Support />
+      </hgroup>
     </div>
-  )
+  );
 }
-export default App
+export default App;
