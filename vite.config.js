e00b066541151c3e454bfef38b9db07c31dfa38c
soohian <shonendumm@gmail.com>
2024-04-27 11:41:46 +0800

    Comment explain base attribute in vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  // use base to tell build to set asset/style links relative to the index.html file
+  // the option here says the asset/styles are in the same folder as index.html file
+  base: './', 
   build: {
     outDir: 'docs' // specify a build out directory for publishing to Github Pages
   }
